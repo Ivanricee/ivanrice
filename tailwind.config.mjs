de23a457717e9fd7b5a6fa9549d07a951f847d4c
Iván Bautista <ivanbautistag@gmail.com>
2023-11-29 16:14:04 -0600

    feat: add custom styles: shadow & backgrounds

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    //"./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        backImg: "#a9b8b7",
+      },
+      backgroundImage: {
+        "floor-rock":
+          "url('https://res.cloudinary.com/ivanrice-c/image/upload/q_61/v1699573269/portfolio/imges/bg_vipxsa.webp')",
+        back: "url https://res.cloudinary.com/ivanrice-c/image/upload/c_scale,q_61/v1700278541/portfolio/imges/bg_shadow_low_ddbgdf.png)",
+      },
+      boxShadow: {
+        "bg-shadow": "-15px -15px 30px #909c9c,15px 15px 30px #b4c3be",
+        "card-xl": "-5.5px -10px 14px #bcc9c0,4px 4px 15px #dae9da",
+        "card-2xl": "-10px -15px 16px #bcc9c0,3px 3px 12px #dae9da",
+      },
+      textColor: {
+        "card-text": "#1c69cc",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
