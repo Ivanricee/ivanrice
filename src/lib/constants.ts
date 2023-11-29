aec4321b67d5fff5b04a257b76ddac3bb03604c4
Iván Bautista <ivanbautistag@gmail.com>
2023-11-29 16:01:22 -0600

    feat: add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const CLOUDINARY_URL =
+  "https://res.cloudinary.com/ivanrice-c/image/upload/";
+export const CLOUDINARY_LOW = "c_scale,q_30,w_400";
+export const CLOUDINARY_HIGH = "c_scale,q_50,w_800";
