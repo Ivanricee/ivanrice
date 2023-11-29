293ca1241c74d0098e5441a0d88e6dc8fa8d389f
Iván Bautista <ivanbautistag@gmail.com>
2023-11-29 16:02:43 -0600

    feat: add react coponent to show info alerts

---
+++ src/components/InfoAlert.tsx
@@ -0,0 +1,13 @@
+import { HiInformationCircle } from "react-icons/hi";
+import { Alert } from "flowbite-react";
+
+export default function InfoAlert({ caption }: { caption: string }) {
+  return (
+    <div className="h-full w.full flex justify-center items-center opacity-70">
+      <Alert color="info" icon={HiInformationCircle}>
+        <span className="font-semibold text-xl">Info alert!</span>
+        <p className="text-lg caption">{caption}</p>
+      </Alert>
+    </div>
+  );
+}
