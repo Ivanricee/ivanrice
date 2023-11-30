9e1a3cd9e5374b5c8beac08af07ce3c9360cec1a
Iván Bautista <ivanbautistag@gmail.com>
2023-11-29 22:04:21 -0600

    feat: refactor & software used map

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,5 @@
 import type { CardInfo } from "../env";
+import { CLOUDINARY_URL } from "./constants";
 
 export const CARD_INFO: CardInfo[] = [
   {
@@ -9,7 +10,7 @@ export const CARD_INFO: CardInfo[] = [
     ],
     title: "CHRISTMAS",
     longTitle: "Little Christmas",
-    software: ["maya", "redshift", "mari", "zbrush", "uv"],
+    software: ["maya", "redshift", "mari", "zbrush", "rizom uv"],
     caption: "Snowman Little Christmas. Rendered with redshift",
     type: "3d",
   },
@@ -18,7 +19,14 @@ export const CARD_INFO: CardInfo[] = [
     urls: ["/v1700182548/portfolio/model/snowman/final_ig_imytwj.webp"],
     title: "SNOWMAN",
     longTitle: "Snowman lights",
-    software: ["maya", "redshift", "mari", "substance3D", "zbrush", "uv"],
+    software: [
+      "maya",
+      "redshift",
+      "mari",
+      "substance 3D",
+      "zbrush",
+      "rizom uv",
+    ],
     caption: "A practice of snow and lighting",
     type: "3d",
   },
@@ -29,10 +37,11 @@ export const CARD_INFO: CardInfo[] = [
     ],
     title: "MICTLANTECUHTLI",
     longTitle: "Mictlantecuhtli - Señor del Mictlan.",
-    software: ["maya", "arnold", "toonShader", "mari"],
+    software: ["maya", "arnold", "mari"],
     caption: "Made with Arnold render.",
     type: "3d",
   },
+
   {
     id: 4,
     urls: [
@@ -44,10 +53,9 @@ export const CARD_INFO: CardInfo[] = [
       "maya",
       "redshift",
       "mari",
-      "substance3D",
-      "substanceD",
+      "substance 3D",
       "zbrush",
-      "uv",
+      "rizom uv",
     ],
     caption: "3D fan art of Ghibli's Totoro, inspired by autumn.  🍂🎨",
     type: "3d",
@@ -59,7 +67,14 @@ export const CARD_INFO: CardInfo[] = [
     ],
     title: "CLANCY",
     longTitle: "Clancy - Fan Art",
-    software: ["maya", "redshift", "mari", "substance3D", "zbrush", "uv"],
+    software: [
+      "maya",
+      "redshift",
+      "mari",
+      "substance 3D",
+      "zbrush",
+      "rizom uv",
+    ],
     caption: "Clancy from The midnight gospel.",
     type: "3d",
   },
@@ -73,13 +88,24 @@ export const CARD_INFO: CardInfo[] = [
       "arnold",
       "nuke",
       "mari",
-      "substance3D",
-      "substanceD",
+      "substance 3D",
       "zbrush",
-      "uv",
+      "rizom uv",
     ],
     caption:
       "steampunk spaceship. Copper material practice with arnold render.",
     type: "3d",
   },
 ];
+
+const transform = "e_grayscale,q_auto:eco";
+export const SOFTWARE: Record<string, string> = {
+  maya: `${CLOUDINARY_URL}${transform}${"/v1609285677/portfolio/icons/maya_mvhgmh.avif"}`,
+  redshift: `${CLOUDINARY_URL}${transform}${"/v1609286796/portfolio/icons/redshift_bzbnze.avif"}`,
+  mari: `${CLOUDINARY_URL}${transform}${"/v1609285677/portfolio/icons/mari_uq9jsd.avif"}`,
+  "substance 3D": `${CLOUDINARY_URL}${transform}${"/v1609285677/portfolio/icons/substance_bpjo6v.avif"}`,
+  zbrush: `${CLOUDINARY_URL}${transform}${"/v1609285678/portfolio/icons/zbrush_fgroar.avif"}`,
+  "rizom uv": `${CLOUDINARY_URL}${transform}${"/v1609285677/portfolio/icons/rizomuv_uaxnda.avif"}`,
+  arnold: `${CLOUDINARY_URL}${transform}${"/v1609285677/portfolio/icons/arnold_fywzrf.avif"}`,
+  nuke: `${CLOUDINARY_URL}${transform}${"/v1609285677/portfolio/icons/nuke_th3pum.avif"}`,
+};
