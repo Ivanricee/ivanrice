693ba0782fd1b45cea56bc44bffefacc6b13f7ff
Iván Bautista <ivanbautistag@gmail.com>
2023-12-04 16:08:19 -0600

    remove react-icons (there are issues with the astro build)

--- src/components/InfoAlert.tsx
+++ src/components/InfoAlert.tsx
@@ -1,10 +1,11 @@
-import { HiInformationCircle } from "react-icons/hi";
+/*import pkg from "react-icons/hi";
+const { HiInformationCircle } = pkg;*/
 import { Alert } from "flowbite-react";
 
 export default function InfoAlert({ caption }: { caption: string }) {
   return (
     <div className="h-full w.full flex justify-center items-center opacity-70">
-      <Alert color="info" icon={HiInformationCircle}>
+      <Alert color="info">
         <span className="font-semibold text-xl">Info alert!</span>
         <p className="text-lg caption">{caption}</p>
       </Alert>
