54e4af4fb1dfe11f657e550d20e75e1706cc2ad8
Iván Bautista <ivanbautistag@gmail.com>
2023-12-04 16:17:37 -0600

    feat: add fadi & fadeOut animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,6 +24,20 @@ export default {
       textColor: {
         "card-text": "#e67177",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.7s ease-in-out",
+        fadeOut: "fadeOut 1s ease-in-out forwards",
+      },
     },
   },
   plugins: [require("flowbite/plugin")],
