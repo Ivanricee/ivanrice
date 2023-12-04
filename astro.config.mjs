5e8b2a6824ee63adc34d2e805dac9d79e0a9a92c
Iván Bautista <ivanbautistag@gmail.com>
2023-12-04 16:19:16 -0600

    add vercel & pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Ivanricee.github.io",
+  base: "/my-repo",
   integrations: [tailwind(), react()],
-  output: "server"
+  output: "server",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
