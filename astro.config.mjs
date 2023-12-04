aa81bc28e94e4e683f1657bac6fa6b1d8941b196
Iván Bautista <ivanbautistag@gmail.com>
2023-12-04 16:19:44 -0600

    feat: add base github  reposit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Ivanricee.github.io",
-  base: "/my-repo",
+  base: "/ivanrice",
   integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel({
