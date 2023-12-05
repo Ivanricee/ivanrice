9cfd82495d247f19d46d0522ffc0ee399de0e286
Iván Bautista <ivanbautistag@gmail.com>
2023-12-04 21:13:05 -0600

    github pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -8,8 +8,9 @@ export default defineConfig({
   site: "https://Ivanricee.github.io",
   base: "/ivanrice",
   integrations: [tailwind(), react()],
-  output: "server",
+  output: "static",
+  /*output: "server",
   adapter: vercel({
     edgeMiddleware: true,
-  }),
+  }),*/
 });
