abb4cee1338dc9d72489c5d5b885f5031e275ffe
Iván Bautista <ivanbautistag@gmail.com>
2023-12-05 15:30:16 -0600

    feat: add images

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -34,6 +34,8 @@ export const CARD_INFO: CardInfo[] = [
     id: 3,
     urls: [
       "/v1700179021/portfolio/model/mictlantecuhtli/mictlantecuhtli_cut_bmmbxs.webp",
+      "/v1700179030/portfolio/model/mictlantecuhtli/mictlantecuhtli_ao_g2ynqp.jpg",
+      "/v1700179026/portfolio/model/mictlantecuhtli/diffuse_ind_iyoy4z.jpg",
     ],
     title: "MICTLANTECUHTLI",
     longTitle: "Mictlantecuhtli - Señor del Mictlan.",
@@ -46,6 +48,9 @@ export const CARD_INFO: CardInfo[] = [
     id: 4,
     urls: [
       "/v1700179626/portfolio/model/Ghibli/ghibli_totoro_autumn_2_bfxe5a.webp",
+      "/v1700179638/portfolio/model/Ghibli/byke_mpidi9.png",
+      "/v1700179681/portfolio/model/Ghibli/lighting_raw_nuhwlq.jpg",
+      "/v1700179664/portfolio/model/Ghibli/REFERENCES_vbdopx.jpg",
     ],
     title: "GHIBLI",
     longTitle: "Ghibli Train Station",
@@ -64,6 +69,9 @@ export const CARD_INFO: CardInfo[] = [
     id: 5,
     urls: [
       "/v1606776535/portfolio/model/midnightgospel/final_img_c_mzsgrf.webp",
+      "/v1606776535/portfolio/model/midnightgospel/sec_dktemn.jpg",
+      "/v1606776535/portfolio/model/midnightgospel/thrids_2_eflw2k.jpg",
+      "/v1700182921/portfolio/model/midnightgospel/gray_shaed_ttscyo.jpg",
     ],
     title: "CLANCY",
     longTitle: "Clancy - Fan Art",
@@ -80,7 +88,14 @@ export const CARD_INFO: CardInfo[] = [
   },
   {
     id: 6,
-    urls: ["/v1606775187/portfolio/model/cyberpunk/nave-final_v6m09q.webp"],
+    urls: [
+      "/v1606775187/portfolio/model/cyberpunk/nave-final_v6m09q.webp",
+      "/v1606774993/portfolio/model/cyberpunk/LIGHT_uu7hiw.jpg",
+      "/v1606774993/portfolio/model/cyberpunk/SPEC_dgjnox.jpg",
+      "/v1606774994/portfolio/model/cyberpunk/emission_qyjzyw.jpg",
+      "/v1606774993/portfolio/model/cyberpunk/UVMAP_utl350.jpg",
+      "/v1606775523/portfolio/model/cyberpunk/ivan-bautista-wip2_hd2o1r.jpg",
+    ],
     title: "SPACESHIP",
     longTitle: "Steampunk spaceship",
     software: [
