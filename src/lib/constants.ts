64ffa5d77a4a09dd321b1aea4f1950e00ec473a8
Iván Bautista <ivanbautistag@gmail.com>
2023-12-19 15:32:10 -0600

    feat: add cloudinary root url for video and transformation options

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,7 @@
 export const CLOUDINARY_URL =
   "https://res.cloudinary.com/ivanrice-c/image/upload/";
+export const CLOUDINARY_VIDEO_URL =
+  "https://res.cloudinary.com/ivanrice-c/video/upload/";
 export const CLOUDINARY_LOW = "c_scale,q_30,w_400";
 export const CLOUDINARY_HIGH = "c_scale,q_50,w_800";
+export const CLOUDINARY_VIDEO = "q_auto,f_auto:video";
