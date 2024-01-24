504d4d1fe7f66dc4654974b9fa1b4fb3c5c2d30d
Iván Bautista <ivanbautistag@gmail.com>
2024-01-23 20:38:06 -0600

    cahmge base from ivanrice to /

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Ivanricee.github.io",
-  base: "/ivanrice",
+  base: "/",
   integrations: [tailwind(), react()],
   output: "static",
   /*output: "server",
