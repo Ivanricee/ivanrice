579a8096e46a0f334563a83b5d08fab42b870184
Iván Bautista <ivanbautistag@gmail.com>
2024-02-05 13:41:51 -0600

    feat: add frontend and 3D constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,5 @@ export const CLOUDINARY_VIDEO_URL =
 export const CLOUDINARY_LOW = "c_scale,q_30,w_400";
 export const CLOUDINARY_HIGH = "c_scale,q_50,w_800";
 export const CLOUDINARY_VIDEO = "q_auto,f_auto:video";
+export const FRONTEND = "frontend";
+export const TRESD = "3D";
