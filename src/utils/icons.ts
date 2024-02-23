699fdd00431e16253239046f7a93ed249c22f687
Iván Bautista <ivanbautistag@gmail.com>
2024-02-22 21:15:30 -0600

    feat: Implement utility for fetching and creating a local icon directory

---
+++ src/utils/icons.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+
+function fetchSVGIcon(icon: string) {
+  const svgPath = path.resolve("public/svg", `${icon}.svg`);
+  try {
+    const svgContent = fs.readFileSync(svgPath, "utf-8");
+    return svgContent;
+  } catch (err) {
+    console.error("SVG read error:", err);
+    return null;
+  }
+}
+const iconDir: Record<string, () => string | null> = {
+  artstation: () => fetchSVGIcon("brand-artstation"),
+  github: () => fetchSVGIcon("brand-github"),
+  gmail: () => fetchSVGIcon("brand-gmail"),
+  instagram: () => fetchSVGIcon("brand-instagram"),
+  linkedin: () => fetchSVGIcon("brand-linkedin"),
+};
+export type IconName = keyof typeof iconDir;
+
+export function getIcon(icon: IconName) {
+  const iconItem = iconDir[icon] ?? null;
+  return iconItem;
+}
