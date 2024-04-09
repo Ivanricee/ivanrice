df87576d820c949fcbfa45933bcbfb3fcde04e33
Iván Bautista <ivanbautistag@gmail.com>
2024-04-09 16:24:20 -0600

    feat: add compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 //import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
+//import react from "@astrojs/react";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Ivanricee.github.io",
   base: "/ivanrice",
-  integrations: [tailwind(), react()],
-  output: "static",
+  integrations: [tailwind(), compressor()],
+  output: "static"
   /*output: "server",
   adapter: vercel({
     edgeMiddleware: true,
