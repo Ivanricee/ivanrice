c0b37c400e9319221e245b2a5288003004a052ff
Iván Bautista <ivanbautistag@gmail.com>
2025-04-08 00:46:11 -0600

    feat add ,pdalData type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,17 +1,27 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-export type Image = {
+export type Media = {
   url: string
   bg: string
   caption?: string
 }
+export type ModalData = {
+  media: Media[]
+  title: string
+  isOpen: boolean
+}
 export interface CardInfo {
   id: number
-  image: Image[]
+  media: Media[]
   title: string
   longTitle: string
   software: string[]
   caption: string
   type: string
   links: { text: string; src: string }[]
+}
+declare global {
+  interface Window {
+    cloudinary: any
+  }
 }
