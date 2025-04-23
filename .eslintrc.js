5ba8e63921dccfb9169c0edcae996615fcead1aa
Iván Bautista <ivanbautistag@gmail.com>
2025-04-23 13:47:33 -0600

    fix sintaxis

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,30 @@ module.exports = {
   // ...
   extends: [
     // ...
-    "plugin:astro/recommended",
-    "prettier"
+    'plugin:astro/recommended',
+    'prettier',
   ],
   // ...
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ["*.astro"],
+      files: ['*.astro'],
       // Allows Astro components to be parsed.
-      parser: "astro-eslint-parser",
+      parser: 'astro-eslint-parser',
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"
-        "max-len": ["error", 140],
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "quotes": [2, "single", { "avoidEscape": true }]
+        'max-len': ['error', 140],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        quotes: [2, 'single', { avoidEscape: true }],
       },
     },
     // ...
