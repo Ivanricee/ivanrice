522d95a1ca1272c33d40865be14b4c036cf6097c
Iván Bautista <ivanbautistag@gmail.com>
2025-04-23 13:23:18 -0600

    refactor: correct DOM tags structure

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,28 +4,19 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { ProjectItem } from './ProjectItem'
 export default function Projects() {
   return (
-    <>
-      <section className="web">
-        <Tabs defaultValue="web" className="w-full">
-          <TabsList className="ml-12 flex w-fit flex-nowrap">
-            <TabsTrigger value="web">Desarrollo Web</TabsTrigger>
-            <TabsTrigger value="tresd">Modelado 3D</TabsTrigger>
-          </TabsList>
-          <TabsContent value="web">
-            <ProjectItem type={FRONTEND} />
-          </TabsContent>
-          <TabsContent value="tresd">
-            <ProjectItem type={TRESD} />
-          </TabsContent>
-        </Tabs>
-      </section>
-    </>
+    <section className="web">
+      <Tabs defaultValue="web" className="w-full">
+        <TabsList className="ml-12 flex w-fit flex-nowrap">
+          <TabsTrigger value="web">Desarrollo Web</TabsTrigger>
+          <TabsTrigger value="tresd">Modelado 3D</TabsTrigger>
+        </TabsList>
+        <TabsContent value="web">
+          <ProjectItem type={FRONTEND} />
+        </TabsContent>
+        <TabsContent value="tresd">
+          <ProjectItem type={TRESD} />
+        </TabsContent>
+      </Tabs>
+    </section>
   )
-}
-
-{
-  /*
-  history.replaceState({}, '', `/ivanrice/#projects/?type=${name}`)
-  const queryString = window.location.hash.split('?')[1]
-  */
 }
